6a0b7a5da518194a71bd466229df9416a2f3a044
Jordan Kline <jordan_kline@epam.com>
2020-10-20 16:32:48 -0700

    Added simple router redirect

--- Angular/angular-mentoring/src/app/app-routing.module.ts
+++ Angular/angular-mentoring/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'courses-list' },
+  { path: 'courses-list', component: AppComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
